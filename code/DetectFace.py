2442897bffbf574dc8b93525d84f31675b7c9c4b
Lucas <lutamaribuchi@gmail.com>
2019-06-21 16:34:49 -0300

    Get descritors work

--- code/DetectFace.py
+++ code/DetectFace.py
@@ -9,32 +9,28 @@ class DetectFace(object):
     """docstring for DetectFace"""
 
     def __init__(self):
-        mypath ='/home/lucas/FacialRecognizer/img/1'
+        currentPath = os.path.abspath(__file__)
+        mypath = os.path.abspath(os.path.join(currentPath, '..', '..', 'img'))
+
         hogHistorygramArray = []
         lbpHistogramArray = []
+
+        hogDescriptor = Hog.getDescriptor()
         filename = ''
         for (root, dirs, files) in os.walk(mypath):
             for name in files:
-                pathOfFile = mypath + "/" + name
+                pathOfFile = os.path.join(mypath, '1', name)
                 face = self.detectFace(pathOfFile)
 
-                # #hogDescriptor = Hog.getDescriptor()
-                # lbpDescriptor = Lbp.getDescriptor(face)
-                # #hogComputed = hogDescriptor.compute(face)
-                # lbpComputed = lbpDescriptor.compute(face)
-                # #hogHistorygramArray.append(hogComputed)
-                # lbpHistogramArray.append(lbpComputed)
+                lbpComputed = Lbp.compute(cv2.cvtColor(face, cv2.COLOR_BGR2GRAY))
 
-        # print(hogHistorygramArray)
-        # print(lbpHistogramArray)
+                hogComputed = hogDescriptor.compute(face)
+                hogHistorygramArray.append(hogComputed)
+                lbpHistogramArray.append(lbpComputed)
 
     @staticmethod
     def detectFace(pathOfFile):
-        face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-        eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-
-        print cv2.__version__
-
+        face_cascade = cv2.CascadeClassifier('C:\\Users\Lucas\AppData\Local\Programs\Python\Python37-32\Lib\site-packages\cv2\data\haarcascade_frontalface_default.xml')
         img = cv2.imread(pathOfFile)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -44,12 +40,6 @@ class DetectFace(object):
 
         for (x, y, w, h) in faces:
             img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            roi_gray = gray[y:y + h, x:x + w]
-            roi_color = img[y:y + h, x:x + w]
-
-            eyes = eye_cascade.detectMultiScale(roi_color)
-            for (ex, ey, ew, eh) in eyes:
-                cv2.rectangle(roi_color, (ex, ey), (ex + ew,ey + eh), (0, 255, 0), 2)
 
         cv2.imshow('img', img)
         cv2.waitKey(0)
